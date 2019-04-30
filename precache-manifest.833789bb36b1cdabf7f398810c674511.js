e877db4af027c153fdfd800a111a29cc19e3fbf3
Mario Kaiser <mario.kaiser@sevenval.com>
2019-04-30 08:23:14 +0200

    deploy

---
+++ precache-manifest.833789bb36b1cdabf7f398810c674511.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "ddb707ac39baadae67c439ae5051c27c",
+    "url": "/assets/android-chrome-512x512.png"
+  },
+  {
+    "revision": "2b69ae341659705dfc30",
+    "url": "/css/app.78e47199.css"
+  },
+  {
+    "revision": "64b82e5cae9c01816070",
+    "url": "/js/chunk-vendors.1c933c75.js"
+  },
+  {
+    "revision": "5b58b1eae4b9c228544b49a9c5bff49a",
+    "url": "/img/showcase.5b58b1ea.jpg"
+  },
+  {
+    "revision": "c642eebd5622e4bc3730ba7f4d44f3db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "2b69ae341659705dfc30",
+    "url": "/js/app.ad627a14.js"
+  },
+  {
+    "revision": "f627ab658a6ebcd92c829dc15d550567",
+    "url": "/assets/og-image.jpg"
+  },
+  {
+    "revision": "a35316d81e0b212dcf10cfe8b72a46bb",
+    "url": "/assets/mstile-150x150.png"
+  },
+  {
+    "revision": "3a1085f67e31882af1b1120993c6a112",
+    "url": "/assets/favicon-32x32.png"
+  },
+  {
+    "revision": "47568c8c25f6e77ca02e00bbb6769181",
+    "url": "/assets/favicon-16x16.png"
+  },
+  {
+    "revision": "38881a630d846543e2436e55fad17b0d",
+    "url": "/assets/browserconfig.xml"
+  },
+  {
+    "revision": "cd2caaf2ab926b055933e56361a227c8",
+    "url": "/assets/apple-touch-icon.png"
+  },
+  {
+    "revision": "ad89f0e0af3c5171ef7fe8d928f8abfd",
+    "url": "/assets/android-chrome-256x256.png"
+  },
+  {
+    "revision": "7ed240cb1c3756ba414b486472f53fc3",
+    "url": "/assets/android-chrome-192x192.png"
+  },
+  {
+    "revision": "3a99cfc102b9ba97a267c3781216218f",
+    "url": "/404.html"
+  }
+];
