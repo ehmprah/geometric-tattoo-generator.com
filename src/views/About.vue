2f2271c0e651991debc7e1e034261654aea4b218
Mario Kaiser <mario.kaiser@sevenval.com>
2019-04-30 09:35:58 +0200

    Prepare netlify deployment

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,9 +4,7 @@
     <img src="../assets/showcase.jpg" alt>
     <p>
       This tattoo generator is a little side project by Mario "ehmprah" Kaiser. I'm a web developer and love tattoos. I proudly wear one tattoo designed
-      <a
-        href="/t/v1/HWNjd"
-      >by this generator</a>. If want to see more of my work, check out my blog at
+      <router-link to="/t/v1/HWNjd">by this generator</router-link>. If want to see more of my work, check out my blog at
       <a
         href="https://frgmnts.blog"
       >frgmnts.blog</a> or follow me on
